e11be983d7a32410632b539cc3b025ebb0814860
Chananphimon Chunchaowarit <164883027+ChananphimonC@users.noreply.github.com>
2025-02-18 22:01:59 +0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Person(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Phone: " + phone);
+    }
+}
