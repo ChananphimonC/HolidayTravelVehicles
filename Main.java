f4b2ac215dec76507c8d09128c89ba4ce7a9dbd0
Chananphimon Chunchaowarit <164883027+ChananphimonC@users.noreply.github.com>
2025-02-18 22:51:51 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Vehicle myVehicle = new Vehicle("Toyota", "Corolla", 2022);
-        myVehicle.displayInfo();
+        
+        Vehicle vehicle = new Vehicle("V12345", "Sedan", "Model S", 2023, "Tesla", 50000);
+        System.out.println("Vehicle Details:");
+        vehicle.displayInfo();
     }
 }
